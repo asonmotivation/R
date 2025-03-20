6c25b42db3e4e400ce78456f77d846985e15e547
Muhammad Shakeel <168127004+asonmotivation@users.noreply.github.com>
2025-03-20 19:47:53 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,18 +4,14 @@
 echo "=== RDP Monitor Service ==="
 echo "Starting up..."
 
-# Install Python dependencies
-pip3 install -q -r requirements.txt
+# Don't try to install packages - they should be pre-installed in the Dockerfile
+# Don't try to change permissions
 
 # Set up environment variables for the monitor script
 export GITHUB_TOKEN=$passwd
 export REPO_OWNER="asonmotivation"
 export REPO_NAME="R"
 export WORKFLOW_NAME="Windows 2022"
-
-# Make sure log directory exists and has right permissions
-mkdir -p /home/user/app/logs
-chmod 777 /home/user/app/logs
 
 # Start the web server in the background
 echo "Starting web interface on port 7860..."
